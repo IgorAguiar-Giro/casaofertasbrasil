d39b9a1c90499fff564e6c7ea84d1d5c421eadc9
Igor Aguiar <80079216+IgorAguiar-Giro@users.noreply.github.com>
2024-07-19 17:28:55 +0000

    Install all dependencies

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="bg-red-500 text-blue-500 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255,255,255,0.17)",
+      },
+    },
+  },
+  plugins: [],
+};
